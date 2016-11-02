68de01871243d6f464ecc37b4d726e2ae12ccc73
bluss <bluss@users.noreply.github.com>
2016-11-02 12:21:30 +0100

    Add benchmark for retain

--- benches/bench.rs
+++ benches/bench.rs
@@ -544,3 +544,36 @@ fn pop_ordermap_100_000(b: &mut Bencher) {
         map
     });
 }
+
+#[bench]
+fn retain_few_ordermap_100_000(b: &mut Bencher) {
+    let map = OMAP_100K.clone();
+
+    b.iter(|| {
+        let mut map = map.clone();
+        map.retain(|k, _| *k % 7 == 0);
+        map
+    });
+}
+
+#[bench]
+fn retain_half_ordermap_100_000(b: &mut Bencher) {
+    let map = OMAP_100K.clone();
+
+    b.iter(|| {
+        let mut map = map.clone();
+        map.retain(|k, _| *k % 2 == 0);
+        map
+    });
+}
+
+#[bench]
+fn retain_many_ordermap_100_000(b: &mut Bencher) {
+    let map = OMAP_100K.clone();
+
+    b.iter(|| {
+        let mut map = map.clone();
+        map.retain(|k, _| *k % 100 != 0);
+        map
+    });
+}
