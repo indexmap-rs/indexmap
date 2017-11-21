a9de40c1629b0a9f16be8c877c78d3354e324b10
Josh Stone <cuviper@gmail.com>
2017-11-21 14:28:31 -0800

    Add macro orderset!{a, b, c, ...}

--- src/macros.rs
+++ src/macros.rs
@@ -36,3 +36,38 @@ macro_rules! ordermap {
         }
     };
 }
+
+#[macro_export]
+/// Create an `OrderSet` from a list of values
+///
+/// ## Example
+///
+/// ```
+/// #[macro_use] extern crate ordermap;
+/// # fn main() {
+///
+/// let set = orderset!{
+///     "a",
+///     "b",
+/// };
+/// assert!(set.contains("a"));
+/// assert!(set.contains("b"));
+/// assert!(!set.contains("c"));
+///
+/// // "a" is the first value
+/// assert_eq!(set.iter().next(), Some(&"a"));
+/// # }
+/// ```
+macro_rules! orderset {
+    ($($value:expr,)+) => { orderset!($($value),+) };
+    ($($value:expr),*) => {
+        {
+            let _cap = ordermap!(@count $($value),*);
+            let mut _set = $crate::OrderSet::with_capacity(_cap);
+            $(
+                _set.insert($value);
+            )*
+            _set
+        }
+    };
+}
--- tests/serde.rs
+++ tests/serde.rs
@@ -20,6 +20,18 @@ fn test_serde() {
 }
 
 #[test]
+fn test_serde_set() {
+    let set = orderset! { 1, 2, 3, 4 };
+    assert_tokens(&set,
+                  &[Token::Seq { len: Some(4) },
+                    Token::I32(1),
+                    Token::I32(2),
+                    Token::I32(3),
+                    Token::I32(4),
+                    Token::SeqEnd]);
+}
+
+#[test]
 fn test_serde_fnv_hasher() {
     let mut map: ::ordermap::OrderMap<i32, i32, ::fnv::FnvBuildHasher> = Default::default();
     map.insert(1, 2);
@@ -31,4 +43,17 @@ fn test_serde_fnv_hasher() {
                     Token::I32(3),
                     Token::I32(4),
                     Token::MapEnd]);
+}
+
+#[test]
+fn test_serde_map_fnv_hasher() {
+    let mut set: ::ordermap::OrderSet<i32, ::fnv::FnvBuildHasher> = Default::default();
+    set.extend(1..5);
+    assert_tokens(&set,
+                  &[Token::Seq { len: Some(4) },
+                    Token::I32(1),
+                    Token::I32(2),
+                    Token::I32(3),
+                    Token::I32(4),
+                    Token::SeqEnd]);
 }
