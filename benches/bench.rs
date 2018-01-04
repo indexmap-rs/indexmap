741708617512407c482dd99849883ca4265a0897
bluss <bluss@users.noreply.github.com>
2018-01-04 19:06:45 +0100

    TEST: Add benchmarks for retain_unordered

--- benches/bench.rs
+++ benches/bench.rs
@@ -590,6 +590,17 @@ fn few_retain_ordermap_100_000(b: &mut Bencher) {
 }
 
 #[bench]
+fn few_retain_unordered_ordermap_100_000(b: &mut Bencher) {
+    let map = OMAP_100K.clone();
+
+    b.iter(|| {
+        let mut map = map.clone();
+        map.retain_unordered(|k, _| *k % 7 == 0);
+        map
+    });
+}
+
+#[bench]
 fn few_retain_hashmap_100_000(b: &mut Bencher) {
     let map = HMAP_100K.clone();
 
@@ -612,6 +623,17 @@ fn half_retain_ordermap_100_000(b: &mut Bencher) {
 }
 
 #[bench]
+fn half_retain_unordered_ordermap_100_000(b: &mut Bencher) {
+    let map = OMAP_100K.clone();
+
+    b.iter(|| {
+        let mut map = map.clone();
+        map.retain_unordered(|k, _| *k % 2 == 0);
+        map
+    });
+}
+
+#[bench]
 fn half_retain_hashmap_100_000(b: &mut Bencher) {
     let map = HMAP_100K.clone();
 
@@ -629,6 +651,17 @@ fn many_retain_ordermap_100_000(b: &mut Bencher) {
     b.iter(|| {
         let mut map = map.clone();
         map.retain(|k, _| *k % 100 != 0);
+        map
+    });
+}
+
+#[bench]
+fn many_retain_unordered_ordermap_100_000(b: &mut Bencher) {
+    let map = OMAP_100K.clone();
+
+    b.iter(|| {
+        let mut map = map.clone();
+        map.retain_unordered(|k, _| *k % 100 != 0);
         map
     });
 }
