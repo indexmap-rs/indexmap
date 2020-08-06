ef0adaf4b47441f709783a7370cb4d63ab19d1a1
Josh Stone <cuviper@gmail.com>
2020-08-06 12:40:58 -0700

    Add a feature to explicitly enable "std"
    If you know you need "std", e.g. relying on default `S = RandomState`,
    then you can enable this feature to force it on, bypassing target
    detection. This may help scenarios where `autocfg` fails detection, like
    issue #144, but at the very least it will speed up the build script by
    avoiding extra `rustc` invocations.

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let ac = autocfg::new();
-    ac.emit_sysroot_crate("std");
+    // If "std" is explicitly requested, don't bother probing the target for it.
+    match std::env::var_os("CARGO_FEATURE_STD") {
+        Some(_) => autocfg::emit("has_std"),
+        None => autocfg::new().emit_sysroot_crate("std"),
+    }
     autocfg::rerun_path("build.rs");
 }
