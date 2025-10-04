3dbac6aafe4cf0f7a1006ff3da224d0a7dfcdb61
Josh Stone <cuviper@gmail.com>
2025-10-04 15:58:03 -0700

    Update the MSRV to 1.82

--- src/lib.rs
+++ src/lib.rs
@@ -79,7 +79,7 @@
 //!
 //! ### Rust Version
 //!
-//! This version of indexmap requires Rust 1.63 or later.
+//! This version of indexmap requires Rust 1.82 or later.
 //!
 //! The indexmap 2.x release series will use a carefully considered version
 //! upgrade policy, where in a later 2.x version, we will raise the minimum
