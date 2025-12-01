9fd9a2990bcabc5412a64053aea5506782ed9d94
Stan Papadopoulos <stan.papadopoulos@proton.me>
2025-12-01 14:54:53 +0100

    Extend testing with ulps_eq

--- test-approxim/src/lib.rs
+++ test-approxim/src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg(test)]
 
-use approxim::{assert_abs_diff_eq, assert_relative_eq};
+use approxim::{assert_abs_diff_eq, assert_relative_eq, assert_ulps_eq};
 use indexmap::indexmap;
 
 #[test]
@@ -13,6 +13,14 @@ fn test_abs_diff_eq() {
     // check different insertion order
     let left = indexmap! { 1 => 2, 3 => 4 };
     let right = indexmap! { 3 => 4, 1 => 2,  };
+    assert_abs_diff_eq!(left, right);
+}
+
+#[test]
+#[should_panic]
+fn test_abs_diff_eq_len() {
+    let left = indexmap! { 1 => 1000, 2 => 2000 };
+    let right = indexmap! { 1 => 1000, 2 => 2000, 3 => 3000 };
     assert_abs_diff_eq!(left, right);
 }
 
@@ -32,4 +40,38 @@ fn test_relative_eq() {
     let left = indexmap! { 1 => 2., 3 => 8. };
     let right = indexmap! { 3 => 8., 1 => 2.,  };
     assert_relative_eq!(left, right);
+}
+
+#[test]
+#[should_panic]
+fn test_relative_eq_len() {
+    let left = indexmap! { 1 => 0., 2 => 1. };
+    let right = indexmap! { 1 => 0., 2 => 1., 3 => 2. };
+    assert_relative_eq!(left, right);
+}
+
+#[test]
+fn test_ulps_eq() {
+    // check with non-default epsilon
+    let left = indexmap! { 1 => 0., 2 => 0. };
+    let right = indexmap! { 1 => 1e-41, 2 => 1e-40 };
+    assert_ulps_eq!(left, right, epsilon = 1e-40);
+
+    // check with non-default max ulps value
+    let left = indexmap! { 1 => 1., 2 => 1. };
+    let right = indexmap! { 1 => 1. + 1e-16, 2 => 1. + 1e-15 };
+    assert_ulps_eq!(left, right, max_ulps = 5);
+
+    // check different insertion order
+    let left = indexmap! { 1 => 2., 3 => 8. };
+    let right = indexmap! { 3 => 8., 1 => 2.,  };
+    assert_ulps_eq!(left, right);
+}
+
+#[test]
+#[should_panic]
+fn test_ulps_eq_len() {
+    let left = indexmap! { 1 => 0., 2 => 1. };
+    let right = indexmap! { 1 => 0., 2 => 1., 3 => 2. };
+    assert_ulps_eq!(left, right);
 }
