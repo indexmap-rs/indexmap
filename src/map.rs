ddb0316dc25e12fb0ecda55f0a4e754dd62e6e5c
Josh Stone <cuviper@gmail.com>
2026-05-01 16:48:15 -0700

    Separate the implementations of `get_disjoint_mut`
    At first glance this may seem worse because it's roughly duplicating the
    `unsafe` code. However, these are now placed in a dedicated module where
    the parallels are apparent, and they should be more direct for their
    respective use-cases.
    
    * For disjoint key lookup in a map, we don't need to produce a `Result`
      that we're just going to turn into panics anyway.
    * For disjoint indices in a slice, we don't need to deal with `Option`,
      and we can drop our own unsafe implementation here in favor of the
      standard library once we update to MSRV 1.86.

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,7 @@
 //! [`IndexMap`] is a hash table where the iteration order of the key-value
 //! pairs is independent of the hash values of the keys.
 
+mod disjoint;
 mod entry;
 mod iter;
 mod mutable;
@@ -952,7 +953,10 @@ where
     ///
     /// ```
     /// let mut map = indexmap::IndexMap::from([(1, 'a'), (3, 'b'), (2, 'c')]);
-    /// assert_eq!(map.get_disjoint_mut([&2, &1]), [Some(&mut 'c'), Some(&mut 'a')]);
+    /// assert_eq!(
+    ///   map.get_disjoint_mut([&2, &1, &0]),
+    ///   [Some(&mut 'c'), Some(&mut 'a'), None],
+    /// );
     /// ```
     #[track_caller]
     pub fn get_disjoint_mut<Q, const N: usize>(&mut self, keys: [&Q; N]) -> [Option<&mut V>; N]
@@ -960,17 +964,8 @@ where
         Q: ?Sized + Hash + Equivalent<K>,
     {
         let indices = keys.map(|key| self.get_index_of(key));
-        match self.as_mut_slice().get_disjoint_opt_mut(indices) {
-            Err(GetDisjointMutError::IndexOutOfBounds) => {
-                unreachable!(
-                    "Internal error: indices should never be OOB as we got them from get_index_of"
-                );
-            }
-            Err(GetDisjointMutError::OverlappingIndices) => {
-                panic!("duplicate keys found");
-            }
-            Ok(key_values) => key_values.map(|kv_opt| kv_opt.map(|kv| kv.1)),
-        }
+        disjoint::get_disjoint_opt_mut(self.as_entries_mut(), indices)
+            .map(|opt| opt.map(Bucket::value_mut))
     }
 
     /// Remove the key-value pair equivalent to `key` and return
